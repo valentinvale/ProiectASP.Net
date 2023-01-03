23eb7c4342e25ef77863c536a87e685210e1bc75
valentinvale <valionsemen@gmail.com>
2023-01-03 03:15:49 +0200

    sa fiu sigur ca s-a salvat

--- ProiectASPNET/ProiectASPNET/Repositories/BookRepository/IBookRepository.cs
+++ ProiectASPNET/ProiectASPNET/Repositories/BookRepository/IBookRepository.cs
@@ -5,12 +5,6 @@ namespace ProiectASPNET.Repositories.BookRepository
 {
     public interface IBookRepository : IGenericRepository<Book>
     {
-        //IEnumerable<Book> GetBooksByAuthor(string author);
-        //IEnumerable<Book> GetBooksByTitle(string title);
-        //IEnumerable<Book> GetBooksByGenre(string genre);
-        //IEnumerable<Book> GetBooksByYearPublished(int year);
-        //IEnumerable<Book> GetBooksByPublisher(string publisher);
-        //IEnumerable<Book> GetBooksByISBN(string isbn);
 
     }
     
