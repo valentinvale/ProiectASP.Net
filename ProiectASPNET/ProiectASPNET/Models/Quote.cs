6cccc64307723c8b3faa9e2db69680e1356e4a3a
valentinvale <valionsemen@gmail.com>
2023-01-14 04:08:38 +0200

    added Quote.cs in models

---
+++ ProiectASPNET/ProiectASPNET/Models/Quote.cs
@@ -0,0 +1,11 @@
+﻿using ProiectASPNET.Models.Base;
+
+namespace ProiectASPNET.Models
+{
+    public class Quote : BaseEntity
+    {
+        public string Text { get; set; }
+        public int UserId { get; set; }
+        
+    }
+}
