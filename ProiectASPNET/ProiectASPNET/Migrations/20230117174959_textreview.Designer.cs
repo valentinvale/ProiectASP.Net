717325eb082a3d2035093ce595830ef89d895d77
valentinvale <valionsemen@gmail.com>
2023-01-17 19:53:25 +0200

    fixed review text

--- ProiectASPNET/ProiectASPNET/Migrations/20230117174959_textreview.Designer.cs
+++ ProiectASPNET/ProiectASPNET/Migrations/20230117174959_textreview.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProiectASPNET.Data;
 
@@ -11,9 +12,11 @@ using ProiectASPNET.Data;
 namespace ProiectASPNET.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20230117174959_textreview")]
+    partial class textreview
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProiectASPNET/ProiectASPNET/Migrations/20230117174959_textreview.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProiectASPNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class textreview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReviewText",
+                table: "Reviews",
+                newName: "Text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Reviews",
+                newName: "ReviewText");
+        }
+    }
+}
