deaa95d5e28e870892d069810cd7c0975f00e4ab
Federicis Alexandru Ionuț <federicis.alexandru@yahoo.com>
2023-01-29 13:48:24 +0200

    token generation and validation

---
+++ ProiectASPNET/ProiectASPNET/Helpers/JwtToken/IJwtUtils.cs
@@ -0,0 +1,10 @@
+﻿using ProiectASPNET.Models;
+
+namespace ProiectASPNET.Helpers.JwtToken
+{
+    public interface IJwtUtils
+    {
+        public string GenerateJwtToken(User user);
+        public Guid ValidateJwtToken(string token);
+    }
+}
